4a8dbea4fb4a3575c6122278d0640104c696364b
alextsoh <alexandrostsochlias@gmail.com>
2026-06-18 16:06:10 +0300

    test url fix

--- App/client-react/src/App.jsx
+++ App/client-react/src/App.jsx
@@ -25,7 +25,7 @@ function CurrentTime(props) {
 
   return (
     <div className="App">
-      <p>API TEST SECURE 1: {data.api}</p>
+      <p>API TEST SECURE 2: {data.api}</p>
       <p>Time from DB : {data.currentTime}</p>
       <p>Request Count: {data.requestCount}</p>
       <div>{isFetching ? "Updating..." : ""}</div>
